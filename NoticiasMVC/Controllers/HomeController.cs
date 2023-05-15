91bcbff917a0bd1b6678eed280324649bf1012fd
jherrera245 <245jh17@gmail.com>
2023-05-14 18:29:39 -0600

    Configuracion de HomeController

--- NoticiasMVC/Controllers/HomeController.cs
+++ NoticiasMVC/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NoticiasMVC.Data;
 using NoticiasMVC.Models;
+using NoticiasMVC.ViewModels;
 using System.Diagnostics;
 
 namespace NoticiasMVC.Controllers
@@ -7,15 +11,41 @@ namespace NoticiasMVC.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            //este objeto contine las Categorias
+            IEnumerable<Categorias> ListaCategorias = _context.Categorias;
+            //pasamos el objeto ListaCategorias a la vista
+            return View(ListaCategorias);
+        }
+
+        public IActionResult Details(int? Id)
+        {
+            //verficamos que se paso el id por la url
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+
+            var Home = new HomeView()
+            {
+                Noticias = _context.Noticias.Include(
+                    i => i.Categoria
+                ).Include(
+                    i => i.Clasificacion
+                ).Where(i => i.IdCategoria == Id),
+                Clasificaciones = _context.Clasificaciones,
+            };
+
+            return View(Home);
         }
 
         public IActionResult Privacy()
---
+++ NoticiasMVC/ViewModels/HomeView.cs
@@ -0,0 +1,10 @@
+﻿using NoticiasMVC.Models;
+
+namespace NoticiasMVC.ViewModels
+{
+    public class HomeView
+    {
+        public IEnumerable<Noticias> Noticias { get; set; }
+        public IEnumerable<Clasificaciones> Clasificaciones { get; set; }
+    }
+}
